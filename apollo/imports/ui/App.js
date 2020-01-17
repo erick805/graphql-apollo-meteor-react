5ccee8293ae8d466ea50c76711a3bec96789b22e
Ricky Lau <rickylaufitness@gmail.com>
2020-01-17 11:05:23 -0500

    add-edit: refactor props from data to graphql

--- apollo/imports/ui/App.js
+++ apollo/imports/ui/App.js
@@ -3,14 +3,13 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import ResolutionForm from "./ResolutionForm";
 
-const App = ({ data }) => {
-  if (data.loading) return null;
+const App = ({ loading, resolutions }) => {
+  if (loading) return null;
   return (
     <>
-      <h1>{data.hi}</h1>
       <ResolutionForm />
       <ul>
-        {data.resolutions.map(resolution => (
+        {resolutions.map(resolution => (
           <li key={resolution._id}>{resolution.name}</li>
         ))}
       </ul>
@@ -27,4 +26,6 @@ const resolutionsQuery = gql`
   }
 `;
 
-export default graphql(resolutionsQuery)(App);
+export default graphql(resolutionsQuery, {
+  props: ({ data }) => ({ ...data })
+})(App);
