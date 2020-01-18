0d11ffd952cbad1b94ba78091f2e5016e2ba5842
Ricky Lau <rickylaufitness@gmail.com>
2020-01-18 15:21:21 -0500

    add: toggleGoal onChange listener to Goal compoenent, add completed into resolutions query, persisitent toggle

--- apollo/imports/ui/resolutions/Goal.js
+++ apollo/imports/ui/resolutions/Goal.js
@@ -3,11 +3,26 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 class Goal extends Component {
+  toggleGoal = () => {
+    const { toggleGoal, goal } = this.props;
+
+    toggleGoal({
+      variables: {
+        id: goal._id
+      }
+    });
+  };
   render() {
+    const { goal } = this.props;
+
     return (
       <li>
-        <input type="checkbox" />
-        {this.props.goal.name}
+        <input
+          type="checkbox"
+          onChange={this.toggleGoal}
+          checked={goal.completed}
+        />
+        {goal.name}
       </li>
     );
   }
